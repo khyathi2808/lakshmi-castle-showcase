533675f5026787707eca8f345e5766b5aedc4f2f
khyathi2808 <khyathi.penjarla@gmail.com>
2025-09-11 13:15:40 +0530

    removed the yesterday's commits

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,43 +1,29 @@
 import Navigation from "@/components/Navigation";
 import HeroSection from "@/components/HeroSection";
-import LatestProjects from "@/components/LatestProjects";
-import Achievements from "@/components/Achievements";
-import QuickLinks from "@/components/QuickLinks";
-import AboutSection from "@/components/AboutSection";
-import ProcessCraftsmanship from "@/components/ProcessCraftsmanship";
 import ProjectHighlights from "@/components/ProjectHighlights";
-import ForHomeowners from "@/components/ForHomeowners";
-import ForInvestors from "@/components/ForInvestors";
-import Resources from "@/components/Resources";
+import AboutSection from "@/components/AboutSection";
 import FloorPlansSection from "@/components/FloorPlansSection";
 import SpecificationsSection from "@/components/SpecificationsSection";
 import GallerySection from "@/components/GallerySection";
 import LocationSection from "@/components/LocationSection";
-import Testimonials from "@/components/testimonials";
-import MediaEngagement from "@/components/MediaEngagement";
 import ContactSection from "@/components/ContactSection";
 import Footer from "@/components/Footer";
+import Testimonials from "@/components/testimonials";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navigation />
       <HeroSection />
-      <LatestProjects />
-      <Achievements />
-      <QuickLinks />
-      <AboutSection />
-      <ProcessCraftsmanship />
       <ProjectHighlights />
-      <ForHomeowners />
-      <ForInvestors />
-      <Resources />
+      <AboutSection />
       <FloorPlansSection />
-      <SpecificationsSection />
+      
       <GallerySection />
       <LocationSection />
-      <Testimonials />
-      <MediaEngagement />
+      
+      <SpecificationsSection />
+      <Testimonials/>
       <ContactSection />
       <Footer />
     </div>
